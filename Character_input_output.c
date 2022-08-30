bbd4cbeb71d35397e6d8c31f6381ed7f94aac60f
Bikal Thapa <88891805+bikalthapa@users.noreply.github.com>
2022-08-30 17:08:00 +0545

    Add files via upload

---
+++ Character_input_output.c
@@ -0,0 +1,11 @@
+/* Character Input Output */
+#include<stdio.h>
+int main(){
+	char a;
+	// This getchar() takes single character input from the user //
+	a= getchar();
+	
+	// This putchar() gives Output to the console //
+	putchar(a);
+	return 0;
+}
