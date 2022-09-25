bae8736137716f706f4326ff58289e14c8deca9f
Bikal Thapa <88891805+bikalthapa@users.noreply.github.com>
2022-09-25 10:28:43 +0545

    Add files via upload

---
+++ Symbolic constant.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define pi 3.14
+int main(){
+	float radius = 12;
+	float area = pi*radius*radius;
+	printf("The area of rectangle is %f",area);
+	return(0);
+	/*
+	Output: 
+	The area of rectangle is 452.160004
+	-------------------------------------
+	*/
+}
