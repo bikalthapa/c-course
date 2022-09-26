8af709440e852388986849dec4fe4279d6ac32e6
Bikal Thapa <88891805+bikalthapa@users.noreply.github.com>
2022-09-26 08:02:35 +0545

    Add files via upload

---
+++ do while loop.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int i = 0;
+	
+	do{
+		printf("My name is Bikal\n");
+		i++;
+	}while(i<100);
+	return(0);
+}
---
+++ while loop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int b;
+	while(b<200){
+		printf("%d\n",b);
+		b++;
+	}
+	return(0);
+}
